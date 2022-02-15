d900322f9da3f3a83afeb966b70803197904f8a7
Anastasiya Litvinenka <loffy@rambler.ru>
2022-02-15 16:26:30 +0300

    header&footer

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,42 +1,44 @@
-import { mapStateToProps } from '../../redux/reducers/mainReducer'
-import Clothes from '../clothes/Clothes'
-import Home from '../home/Home'
-import Product from '../product/Product'
-import ProductsHeader from '../products-header/ProductsHeader'
-import RelatedProducts from '../relatedProducts/RelatedProducts'
+// import { mapStateToProps } from '../../redux/reducers/mainReducer'
+// import Clothes from '../clothes/Clothes'
+// import Home from '../home/Home'
+// import Product from '../product/Product'
+// import ProductsHeader from '../products-header/ProductsHeader'
+// import RelatedProducts from '../relatedProducts/RelatedProducts'
 import './Main.css'
 import { connect } from 'react-redux'
 
-const Main = ({ page, productType, women, men, filtered, preview }) => {
+const Main = (
+    // { page, productType, women, men, filtered, preview }
+) => {
     return <main>!!!</main>
 
-    let products = {}
-    productType === 'women' ? products = women : products = men
+    // let products = {}
+    // productType === 'women' ? products = women : products = men
 
-    const related = filtered.slice(0, 4)
+    // const related = filtered.slice(0, 4)
 
-    if (page === 'home') return (
-        <main><Home /></main>)
+    // if (page === 'home') return (
+    //     <main><Home /></main>)
 
-    else if (page === 'product') return (
-        <main>
-            <Product
-                productType={productType}
-                products={products}
-                preview={preview}
-            />
-            <RelatedProducts related={related} productType={productType} /></main>)
+    // else if (page === 'product') return (
+    //     <main>
+    //         <Product
+    //             productType={productType}
+    //             products={products}
+    //             preview={preview}
+    //         />
+    //         <RelatedProducts related={related} productType={productType} /></main>)
 
 
-    else if (page === 'products') return (
-        <main>
-            <ProductsHeader title={productType} />
-            <Clothes
-                productType={productType}
-                products={products}
-            />
-        </main>
-    )
+    // else if (page === 'products') return (
+    //     <main>
+    //         <ProductsHeader title={productType} />
+    //         <Clothes
+    //             productType={productType}
+    //             products={products}
+    //         />
+    //     </main>
+    // )
 
 }
 
